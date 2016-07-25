bd559d74c092ec4094a52b6105372ab1f7b17cdc
tbl0605 <thierryblind@msn.com>
2016-07-25 16:40:14 +0200

    Update versions of JQuery and jquery-validation

--- application-demo/views/__templates/footer.php
+++ application-demo/views/__templates/footer.php
@@ -2,10 +2,10 @@
 <script type="text/javascript" charset="UTF-8"
 	src="<?=URL_PUB?>js/jquery-loading/1.2.0/jquery.loading<?=YAM_MIN?>.js"></script>
 <script type="text/javascript" charset="UTF-8"
-	src="<?=URL_PUB?>js/jquery-validation/1.15.0/jquery.validate<?=YAM_MIN?>.js"></script>
+	src="<?=URL_PUB?>js/jquery-validation/1.15.1/jquery.validate<?=YAM_MIN?>.js"></script>
 <script type="text/javascript" charset="UTF-8"
-	src="<?=URL_PUB?>js/jquery-validation/1.15.0/additional-methods<?=YAM_MIN?>.js"></script>
+	src="<?=URL_PUB?>js/jquery-validation/1.15.1/additional-methods<?=YAM_MIN?>.js"></script>
 <!-- <script type="text/javascript" charset="UTF-8"
-	src="<?=URL_PUB?>js/jquery-validation/1.15.0/localization/messages_fr.js"></script> -->
+	src="<?=URL_PUB?>js/jquery-validation/1.15.1/localization/messages_fr.js"></script> -->
 </body>
 </html>
