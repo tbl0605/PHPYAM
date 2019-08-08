882d25dc54f334d10a45599b1b22448c33300e8a
tbl0605 <tbl0605@gmail.com>
2019-08-08 16:20:08 +0200

    Minor demo code cleanup: do not add newline after closing </html> tag

--- application-demo-autoloader/views/__templates/footer.php
+++ application-demo-autoloader/views/__templates/footer.php
@@ -16,7 +16,7 @@ use PHPYAM\demo\confs\AppConfig;
  * - be tested to display data without using any controller
  * A view MUST NOT:
  * - directly access to the context of the web application (variables $_GET, $_POST, $_SESSION, $GLOBALS, etc...),
- *   because the calling controller is responsible for that!
+ * because the calling controller is responsible for that!
  */
 class Footer
 {
@@ -34,8 +34,7 @@ class Footer
 <!-- <script type="text/javascript" charset="UTF-8"
 	src="<?=AppConfig::URL_PUB?>js/jquery-validation/1.15.1/localization/messages_fr.js"></script> -->
 </body>
-</html>
-<?php
+</html><?php
     }
 }
 ?>
